c821a18c7ad9cbb34a8a5994c90857d7f45ab22e
Tahseen <tahseenislam@hotmail.com>
2022-04-04 22:08:02 +1000

    added cypress tests

--- cypress/integration/search_spec.js
+++ cypress/integration/search_spec.js
@@ -1,19 +1,46 @@
-describe('user can successfully see top rated popular and trending movies', () => {
-  it('user can successfully see top rated popular and trending movies', () => {
-    // click any one of the buttons
-    // see that it is loading movies
+describe('User Experience Test 1', () => {
+  it('User Can search a movie and search by pressing enter or clicking the search button', () => {
     cy.visit('http://localhost:3000/movie-search-react');
     cy.get('input[name="searchbar"]').click().type('batman{enter}')
-    // cy.get('#root > div > main > header > div > div > div > span > svg').click()
-    // cy.get('input[name="searchbar"]').click().type('spiderman')
-    // cy.findByText(/top rated/i).click()
-    // cy.wait(2000)
+    cy.wait(2000)
+    cy.get('input[name="searchbar"]').clear()
+    cy.get('input[name="searchbar"]').click().type('spiderman')
+    cy.get('#root > div > main > header > div > div > div > span > svg').click()
+  })
+})
+
+
+describe('User Experience Test 2', () => {
+  it('User can click on a movie to get more information', () => {
+    cy.visit('http://localhost:3000/movie-search-react');
+    cy.get('input[name="searchbar"]').click().type('batman{enter}')
+    cy.wait(2000)
+    cy.findByRole('link', { name: /the batman rating: 7\.9/i }).click()
+  })
+})
+
+describe('User Experience Test 3', () => {
+  it('User can click and see results for popular, trending and top rated', () => {
+    cy.visit('http://localhost:3000/movie-search-react');
+    cy.findByText(/top rated/i).click()
+    cy.wait(2000)
     // cy.findByRole('combobox').select(1)
     // cy.wait(2000)
     // cy.findByRole('combobox').select(2)
-    // cy.findByText(/trending/i).click()
-    // cy.wait(2000)
-    // cy.findByText(/popular/i).click()
-    // cy.wait(2000)
+    cy.findByText(/trending/i).click()
+    cy.wait(2000)
+    cy.findByText(/popular/i).click()
+    cy.wait(2000)
+  })
+})
+
+describe('User Experience Test 4', () => {
+  it('User can sort by lowest and highest rating', () => {
+    cy.visit('http://localhost:3000/movie-search-react');
+    cy.findByText(/top rated/i).click()
+    cy.wait(2000)
+    cy.findByRole('combobox').select(1)
+    cy.wait(2000)
+    cy.findByRole('combobox').select(2)
   })
 })
